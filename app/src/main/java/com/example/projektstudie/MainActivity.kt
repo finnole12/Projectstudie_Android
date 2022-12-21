b8e81e7be20478340e6670d66ff70f63db8a323e
I548329 <finnolefinnole@yahoo.de>
2022-12-21 14:25:31 +0100

    fix viewbinding

--- app/src/main/java/com/example/projektstudie/MainActivity.kt
+++ app/src/main/java/com/example/projektstudie/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.projektstudie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projektstudie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: MainActivityBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
